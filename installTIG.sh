f7aeed402226570371b7bdffb6d4659f2156cd73
Roman Popov / Ceron / LordOfElectee <55095076+LordOfElectee@users.noreply.github.com>
2022-01-12 00:27:10 +0500

    Update installTIG.sh

--- installTIG.sh
+++ installTIG.sh
@@ -76,6 +76,18 @@ echo "можно вводить несколько по возрастанию 1
 read -p "Введите вариант установки: " setup
 read -p "Введите пароль для TIG: " pass
 read -p "Устанавливать время + mc + ncdu + make + net-tools + git + jq? (y/n) " base
+
+case $base in
+y) apt install mc ncdu net-tools make git jq ufw -y
+   apt update && apt upgrade -y
+   timedatectl set-timezone Asia/Yekaterinburg
+   sudo sed -i 's|# en_US.UTF-8 UTF-8|en_US.UTF-8 UTF-8|g' /etc/locale.gen
+   sudo sed -i 's|# ru_RU.UTF-8 UTF-8|ru_RU.UTF-8 UTF-8|g' /etc/locale.gen
+   sudo locale-gen
+   sudo localectl set-locale LANG=ru_RU.UTF-8;;
+*) echo "Вышли из установки";;
+esac
+
 case $setup in
 1) installInfluxDB;;
 2) installTelegraf
@@ -96,15 +108,4 @@ case $setup in
     changeTelegrafConfig
     createDatabase;;
 *) echo "Нет такой опции";;
-esac
-
-case $base in
-y) apt install mc ncdu net-tools make git jq -y
-   apt update && apt upgrade -y
-   timedatectl set-timezone Asia/Yekaterinburg
-   sudo sed -i 's|# en_US.UTF-8 UTF-8|en_US.UTF-8 UTF-8|g' /etc/locale.gen
-   sudo sed -i 's|# ru_RU.UTF-8 UTF-8|ru_RU.UTF-8 UTF-8|g' /etc/locale.gen
-   sudo locale-gen
-   sudo localectl set-locale LANG=ru_RU.UTF-8;;
-*) echo "Вышли из установки";;
 esac
