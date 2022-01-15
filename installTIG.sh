9e835a638b85981ce37934abbc6bf52de2e95de5
Roman Popov / Ceron / LordOfElectee <55095076+LordOfElectee@users.noreply.github.com>
2022-01-16 00:55:27 +0500

    Update installTIG.sh

--- installTIG.sh
+++ installTIG.sh
@@ -78,7 +78,7 @@ read -p "Введите пароль для TIG: " pass
 read -p "Устанавливать время + mc + ncdu + make + net-tools + git + jq? (y/n) " base
 
 case $base in
-y) apt install mc ncdu net-tools make git jq ufw -y
+y) apt install mc ncdu net-tools make git jq ufw htop nano -y
    apt update && apt upgrade -y
    timedatectl set-timezone Asia/Yekaterinburg
    sudo sed -i 's|# en_US.UTF-8 UTF-8|en_US.UTF-8 UTF-8|g' /etc/locale.gen
