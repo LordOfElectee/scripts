2e61cdf671963d7e5d95c24d5fbbca092d4c0acb
Roman Popov / Ceron / LordOfElectee <55095076+LordOfElectee@users.noreply.github.com>
2022-02-19 18:05:01 +0500

    Update installgo.sh

--- installgo.sh
+++ installgo.sh
@@ -1,8 +1,10 @@
 cd $HOME
+apt update
 wget -O go1.17.4.linux-amd64.tar.gz https://golang.org/dl/go1.17.4.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.4.linux-amd64.tar.gz && rm go1.17.4.linux-amd64.tar.gz
 echo 'export GOROOT=/usr/local/go' >> $HOME/.bash_profile
 echo 'export GOPATH=$HOME/go' >> $HOME/.bash_profile
 echo 'export GO111MODULE=on' >> $HOME/.bash_profile
 echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile && . $HOME/.bash_profile
+apt install gcc
 go version
